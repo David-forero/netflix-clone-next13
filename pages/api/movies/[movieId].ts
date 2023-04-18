98d939c9e790f50485c2f065c6cfa45fa97bfe3e
David Forero <54685136+David-forero@users.noreply.github.com>
2023-04-18 10:41:12 -0400

    final project

---
+++ pages/api/movies/[movieId].ts
@@ -0,0 +1,36 @@
+import { NextApiResponse, NextApiRequest } from "next";
+import serverAuth from "../../../lib/serverAuth";
+import prismadb from "../../../lib/prismadb";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== "GET") {
+    return res.status(405).end();
+  }
+
+  try {
+    await serverAuth(req, res);
+    const { movieId } = req.query;
+
+    if (typeof movieId !== "string") {
+      throw new Error("Invalid Id");
+    }
+
+    if (!movieId) {
+      throw new Error("Invalid Id");
+    }
+
+    const movie = await prismadb.movie.findUnique({
+      where: {
+        id: movieId,
+      },
+    });
+
+    return res.status(200).json(movie)
+  } catch (error) {
+    console.error(error);
+    return res.status(400).end();
+  }
+}
